9f7d616105cec7c9bb85dd1bf4ee1a76247f9eee
YSChan <65056212+yschan8137@users.noreply.github.com>
2024-01-07 21:35:12 +0800

    Add merge_sublist function to Loan package and update bar_data calculation in test_for_app.ipynb

--- app/Loan/__init__.py
+++ app/Loan/__init__.py
@@ -1,4 +1,5 @@
 # __all__ = ['main']
 from .main import calculator, df_schema, default_kwargs, example_for_subsidy_arr
+from .computation.helpers.scheduler import merge_sublist
 from .information import _591_, moi
 from app.Loan.computation.categories import amortization as amortization_methods
