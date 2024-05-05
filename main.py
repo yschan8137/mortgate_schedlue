0abb6fc13208e80281d861d23b4d7540f52573b0
YSChan <65056212+yschan8137@users.noreply.github.com>
2024-05-05 19:38:19 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
+
+
 if __name__ == "__main__":
+    app.run_server(
+        debug=True,
+        # threaded=True, 
+        # dev_tools_ui=True,
+        # dev_tools_hot_reload= True,
+        # threaded=True,
+        # port= 8050,
+        # dev_tools_hot_reload_interval= 1000,
+    )
